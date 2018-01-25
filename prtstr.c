a492f2c4784875f542174e2da8302324ab6c4f20
vaishalivsb <31504057+vaishalivsb@users.noreply.github.com>
2018-01-25 11:19:55 +0000

    Update and rename strcon.c to prtstr.c

---
+++ prtstr.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+char N[56]="Laptop";
+  printf("%s",N);
+  scanf("%d",&K);
+  printf("%d",K);
+  for(i=1;i<=K;i++)
+    printf("Laptop\n");
+  return 0;
+}
--- strcon.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main()
-{
-char a[56],b[40];
-  scanf("%s%s",&a,&b);
-  printf("%s%s",a,b);
-  return 0;
-}
